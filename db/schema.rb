b84a36adaff3db67880a95b19c6c17b52398862e
LionelGies <lionelgies0119@gmail.com>
2015-03-24 04:23:48 +0100

    Spree_fancy Theme install

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324030746) do
+ActiveRecord::Schema.define(version: 20150324032103) do
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
