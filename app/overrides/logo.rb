4650e641ca440a6157c7cb09a807c4bce1c9d414
LionelGies <lionelgies0119@gmail.com>
2015-03-25 02:58:57 +0100

    logo link add

--- app/overrides/logo.rb
+++ app/overrides/logo.rb
@@ -2,5 +2,5 @@ Deface::Override.new(:virtual_path => 'spree/shared/_header',
 	:name => "logo",
 	:replace_contents => '#logo',
 	:text =>
-	"<img src=\"/logo/logo.png\" alt=\"Lionel Gies Logo\"/>"
+	"<%= link_to( image_tag(\"/logo/logo.png\"), spree.root_path ) %>"
 )
